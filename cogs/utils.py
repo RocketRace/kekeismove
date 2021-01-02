1fc9e43b3e298dec65b75ada66b53aad30f3f9fd
Olivia Palmu <oliviaspalmu@gmail.com>
2021-01-02 11:45:58 +0900

    fix staff checks failing

--- cogs/utils.py
+++ cogs/utils.py
@@ -3,17 +3,17 @@ from cogs.errors import *
 from discord.ext import commands
 
 async def has_staff_permissions(ctx: commands.Context) -> bool:
-    return discord.utils.find(
-        lambda r: str(r.id) == ctx.bot.settings["staff_role"],
-        ctx.author.roles
+    return discord.utils.get(
+        ctx.author.roles,
+        id=ctx.bot.settings["staff_role"]
     ) is not None or await ctx.bot.is_owner(ctx.author)
 
 def mention_role(ctx: commands.Context):
-    def inner(role: str):
-        return ctx.guild.get_role(int(role)).mention
+    def inner(role: int):
+        return ctx.guild.get_role(role).mention
     return inner
 
 def members(ctx: commands.Context):
-    def inner(role: str):
-        return len(ctx.guild.get_role(int(role)).members)
+    def inner(role: int):
+        return len(ctx.guild.get_role(role).members)
     return inner
